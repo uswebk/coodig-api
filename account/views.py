77fdbd8f3385c5b74aa5516997114ca2333c1eb0
uswebk <usweb111@gmail.com>
2022-12-30 22:32:33 +0900

    [feature] api routing

--- account/views.py
+++ account/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 
 # Create your views here.
+class TestView(APIView):
+    permission_classes = [permissions.AllowAny, ]
+
+    def get(self, request):
+        return Response({'message': 'Request Success!'})
+
+    def post(self, request):
+        pass
