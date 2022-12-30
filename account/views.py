e2f33bb66793c285461996100109c354a3a9e99e
uswebk <usweb111@gmail.com>
2022-12-31 02:49:35 +0900

    [feature] send email about otp

--- account/views.py
+++ account/views.py
@@ -1,4 +1,5 @@
 import datetime
+import os
 import random
 
 from django.utils import timezone
@@ -7,12 +8,12 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.generics import ListAPIView, RetrieveAPIView
+from django.core.mail import EmailMessage
 
 from account.models import Account, Otp
 from account.serializers import AccountRegistrationSerializer, AccountSerializer, OtpSerializer
 
 
-# Create your views here.
 def get_tokens_for_user(user):
     refresh = RefreshToken.for_user(user)
     return {
@@ -29,12 +30,27 @@ class RegistrationView(APIView):
         serializer.is_valid(raise_exception=True)
         account = serializer.save()
         token = get_tokens_for_user(account)
-        Otp.objects.create(
+        otp = Otp.objects.create(
             code=str(random.randrange(0, 999999)).zfill(6),
             expiration_date=timezone.now() + datetime.timedelta(minutes=10),
             account_id=account.id
         )
-        # Otp Send Email
+
+        # TODO:Email Service
+        body = 'OPT: ' + otp.code
+        data = {
+            'subject': 'Reset Your Password',
+            'body': body,
+            'to_email': account.email
+        }
+
+        email = EmailMessage(
+            subject=data['subject'],
+            body=data['body'],
+            from_email=os.environ.get('EMAIL_FROM'),
+            to=[data['to_email']]
+        )
+        email.send()
 
         return Response({'token': token}, status=status.HTTP_201_CREATED)
 
