e3314569ae116355e16e5056d683b4071c173539
uswebk <usweb111@gmail.com>
2022-12-30 20:59:04 +0900

    [update] connect mysql

--- coodig/settings.py
+++ coodig/settings.py
@@ -73,11 +73,17 @@ WSGI_APPLICATION = 'coodig.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'coodig',
+        'USER': 'coodig',
+        'PASSWORD': 'coodig',
+        'HOST': '127.0.0.1',
+        'PORT': '3309',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        }
     }
 }
 
