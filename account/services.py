6639d5f15cf2fc86c8cc546fe8b6b801cd2c2d42
uswebk <usweb111@gmail.com>
2023-01-03 18:15:12 +0900

    [refactor] otp verify service

--- account/services.py
+++ account/services.py
@@ -1,9 +1,11 @@
 import datetime
 import random
 
+from django.db.models import Prefetch
 from django.utils import timezone
 
 from account.consts import OTP_VALID_MINUTES, OTP_CODE_NUMBER_OF_DIGITS
+from account.exceptions import OtpVerifyError
 from account.models import Otp, Account
 
 
@@ -26,3 +28,22 @@ class OtpService:
         for num in range(self.number_of_digits):
             digits += '9'
         return int(digits)
+
+
+def otp_verify(email: str, otp_code: str) -> bool:
+    account = Account.objects.filter(
+        email=email, email_verified_at__isnull=True).prefetch_related(
+        Prefetch('otp_set', queryset=Otp.objects.filter(expiration_date__gte=timezone.now()).all(),
+                 to_attr="otps")).first()
+    if account is None:
+        raise OtpVerifyError('invalid otp verify')
+    otps = account.otps
+    if not otps:
+        raise OtpVerifyError('invalid otp verify')
+    otp = otps[-1]
+    if otp.code != otp_code:
+        raise OtpVerifyError('wrong otp code')
+    account.email_verified_at = timezone.now()
+    account.save()
+
+    return True
--- account/views.py
+++ account/views.py
@@ -1,9 +1,4 @@
-import datetime
-import random
-
 from django.db import transaction
-from django.db.models import Prefetch
-from django.utils import timezone
 from rest_framework import permissions, status
 from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
@@ -12,9 +7,9 @@ from rest_framework_simplejwt.tokens import RefreshToken
 
 from account.email import send_opt
 from account.exceptions import OtpVerifyError
-from account.models import Account, Otp
+from account.models import Account
 from account.serializers import AccountRegistrationSerializer, AccountSerializer, VerifyAccountSerializer
-from account.services import OtpService
+from account.services import OtpService, otp_verify
 
 
 def get_tokens_for_user(user):
@@ -46,23 +41,8 @@ class VerifyOtp(APIView):
     def post(self, request):
         serializer = VerifyAccountSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
-            email = serializer.data['email']
-            account = Account.objects.filter(
-                email=email, email_verified_at__isnull=True).prefetch_related(
-                Prefetch('otp_set', queryset=Otp.objects.filter(expiration_date__gte=timezone.now()).all(),
-                         to_attr="otps")).first()
             try:
-                if account is None:
-                    raise OtpVerifyError('invalid otp verify')
-                otps = account.otps
-                if not otps:
-                    raise OtpVerifyError('invalid otp verify')
-                otp_code = serializer.data['otp']
-                otp = otps[-1]
-                if otp.code != otp_code:
-                    raise OtpVerifyError('wrong otp code')
-                account.email_verified_at = timezone.now()
-                account.save()
+                otp_verify(serializer.data['email'], serializer.data['otp'])
                 return Response({"message": "otp verify success"}, status=status.HTTP_200_OK)
             except OtpVerifyError as e:
                 return Response({"message": str(e)}, status=status.HTTP_400_BAD_REQUEST)
