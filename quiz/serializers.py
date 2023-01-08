86f95b5cce4ecd9154d397e8a984c377277896bc
uswebk <usweb111@gmail.com>
2023-01-09 03:47:56 +0900

    [feature] quiz create in serializer

--- quiz/serializers.py
+++ quiz/serializers.py
@@ -21,6 +21,23 @@ class QuizSerializer(serializers.ModelSerializer):
     def get_choices(obj):
         return QuizChoiceSerializer(QuizChoice.objects.all().filter(quiz_id=obj.id), many=True).data
 
+    def validate(self, attrs):
+        result = dict()
+        attrs['created_by'] = self.context['request'].user
+        result['quiz'] = attrs
+        result['choices'] = self.context['request'].data['choices']
+
+        return result
+
+    def create(self, validate_data):
+        quiz = Quiz.objects.create(**validate_data['quiz'])
+        for choice in validate_data['choices']:
+            choice['quiz_id'] = quiz.id
+            quiz_choice_serializer = QuizChoiceSerializer(data=choice)
+            quiz_choice_serializer.is_valid(raise_exception=True)
+            quiz_choice_serializer.save()
+        return quiz
+
 
 class QuizChoiceSerializer(serializers.ModelSerializer):
     class Meta:
