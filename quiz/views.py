5f7d5eb5348c831fa11680796d6cc3a572503636
uswebk <usweb111@gmail.com>
2023-01-08 22:56:18 +0900

    [feature] tag list & create view

--- quiz/views.py
+++ quiz/views.py
@@ -1,5 +1,11 @@
+from rest_framework import permissions
 from rest_framework.generics import ListCreateAPIView
+
+from quiz.models import Tag
+from quiz.serializers import TagSerializer
 
 
 class TagListView(ListCreateAPIView):
-    pass
+    permission_classes = [permissions.IsAuthenticated, ]
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
