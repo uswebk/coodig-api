946e99a103ad6d5319f1b296ea78a1b9b603dd7f
uswebk <usweb111@gmail.com>
2023-01-08 19:24:43 +0900

    [feature] add quiz tag table

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from account.models import Account
+
+
+class Tag(models.Model):
+    class Meta:
+        db_table = 'tags'
+
+    created_by = models.ForeignKey(Account, db_column='created_by', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    color = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
