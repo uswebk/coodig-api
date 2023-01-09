c070cc693a6c2f68e36554e943db52560d1b3933
uswebk <usweb111@gmail.com>
2023-01-10 01:29:03 +0900

    [feature] add quiz_answers table

--- quiz/models.py
+++ quiz/models.py
@@ -47,3 +47,16 @@ class QuizTag(models.Model):
 
     class Meta:
         db_table = 'quizzes_tags'
+
+
+class QuizAnswer(models.Model):
+    account_id = models.ForeignKey(Account, related_name='answer_account', db_column='account_id',
+                                   on_delete=models.CASCADE)
+    quiz_id = models.ForeignKey(Quiz, related_name='answer_quiz', db_column='quiz_id', on_delete=models.CASCADE)
+    question = models.TextField()
+    is_correct = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'quiz_answers'
