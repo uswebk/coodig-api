ea3b03f52cb4400a94e50c0f318e9942f031a239
uswebk <usweb111@gmail.com>
2023-09-13 20:24:01 +0900

    [refactor] delete not use import

--- quiz/api/views.py
+++ quiz/api/views.py
@@ -1,11 +1,11 @@
 import datetime
 
 from django.db import transaction
-from django.db.models import Count, Sum, Q
+from django.db.models import Count, Q
 from django.http import Http404
 from rest_framework import status
 from rest_framework.decorators import action
-from rest_framework.generics import get_object_or_404, ListAPIView
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
