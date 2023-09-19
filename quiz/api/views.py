2a087ef81b9e9c71ae315017b2fe0d3de9c7dcd6
uswebk <usweb111@gmail.com>
2023-09-20 00:11:17 +0900

    [refactor] delete api

--- quiz/api/views.py
+++ quiz/api/views.py
@@ -9,7 +9,7 @@ from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
-from quiz.models import Tag, Quiz, QuizAnswer, QuizAnswerChoice
+from quiz.models import Tag, Quiz, QuizAnswer
 from quiz.api.serializers import TagSerializer, QuizSerializer, QuizAnswerSerializer
 from quiz.services import QuizService, RandomQuizServie
 
@@ -21,34 +21,43 @@ class TagViewSet(ModelViewSet):
 
 class QuizViewSet(ModelViewSet):
     serializer_class = QuizSerializer
+    queryset = Quiz.objects.all()
 
     def get_queryset(self):
         user = self.request.user
-        return Quiz.objects.filter(created_by=user).prefetch_related('choices')
+        return self.queryset.filter(created_by=user).prefetch_related('choices')
+
+    @action(methods=['GET'], detail=False)
+    def random(self, request):
+        limit = int(request.GET.get('limit')) if request.GET.get('limit') is not None else 10
+        quiz = RandomQuizServie.get_random(self.request.user, limit)
+
+        quiz = self.queryset.random(self.request.user, limit)
+
+        if not quiz:
+            raise Http404
+        return Response(self.serializer_class(quiz, many=True).data, status=status.HTTP_200_OK)
+
+
+class AnswerViewSet(ModelViewSet):
+    serializer_class = QuizAnswerSerializer
+
+    def get_queryset(self):
+        account = self.request.user
+        queryset = QuizAnswer.objects.filter(account_id=account.id).prefetch_related('answer_choices')
+
+        start_date = self.request.query_params.get('start_date')
+        if start_date:
+            queryset = queryset.filter(created_at__gte=start_date)
+
+        end_date = self.request.query_params.get('end_date')
+        if end_date:
+            queryset = queryset.filter(created_at__lte=end_date)
+
+        return queryset
 
     def create(self, request, *args, **kwargs):
-        with transaction.atomic():
-            account = self.request.user
-            data = request.data['quiz']
-            data['created_by'] = account.id
-            quiz_serializer = self.get_serializer(data=data)
-            quiz_serializer.is_valid(raise_exception=True)
-            quiz = quiz_serializer.save()
-
-            quiz_service = QuizService(quiz)
-            quiz_service.create_choices(request.data['quiz']['choices'])
-            quiz_service.create_tags(request.data['tags'], account)
-
-        return Response(quiz_serializer.data, status=status.HTTP_201_CREATED)
-
-    def destroy(self, request, *args, pk=None):
-        quiz = get_object_or_404(Quiz, pk=pk, created_by=self.request.user, is_deleted=False)
-        quiz.is_deleted = True
-        quiz.save()
-        return Response({}, status=status.HTTP_200_OK)
-
-    @action(methods=['POST'], detail=True)
-    def answer(self, request, pk=None):
+        pk = request.data['quiz_id'] or 0
         quiz = get_object_or_404(Quiz.objects.prefetch_related('choices'), pk=pk)
         quiz_choices = quiz.choices.all()
 
@@ -74,33 +83,6 @@ class QuizViewSet(ModelViewSet):
         serializer.save()
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-    @action(methods=['GET'], detail=False)
-    def random(self, request):
-        limit = int(request.GET.get('limit')) if request.GET.get('limit') is not None else 10
-        quiz = RandomQuizServie.get_random(self.request.user, limit)
-
-        if not quiz:
-            raise Http404
-        return Response(self.serializer_class(quiz, many=True).data, status=status.HTTP_200_OK)
-
-
-class AnswerViewSet(ModelViewSet):
-    serializer_class = QuizAnswerSerializer
-
-    def get_queryset(self):
-        account = self.request.user
-        queryset = QuizAnswer.objects.filter(account_id=account.id).prefetch_related('answer_choices')
-
-        start_date = self.request.query_params.get('start_date')
-        if start_date:
-            queryset = queryset.filter(created_at__gte=start_date)
-
-        end_date = self.request.query_params.get('end_date')
-        if end_date:
-            queryset = queryset.filter(created_at__lte=end_date)
-
-        return queryset
 
     @action(methods=['GET'], detail=False)
     def stats(self, request):
